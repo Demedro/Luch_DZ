14de515a21cbd32ed4fcba7cd9753bd94d168ab3
Demedro <aneox@list.ru>
2021-02-09 03:13:37 +0300

    Задание по бинарному поиску

--- DZ_3_1.py
+++ DZ_3_1.py
@@ -1,4 +1,24 @@
 import random
 
-a = random.randint(1, 100)
-print(a)
+a = []
+c = int(input('введите размер выборки '))
+for r in range(c):
+    a.append(random.randint(1, 100))
+
+a.sort()
+b = int(input('введите число '))
+it = c
+while it > 1:
+    n = round(it / 2)
+    if b < a[n]:
+        ans = a[n]
+        a = a[:n]
+        it = len(a)
+    elif b > a[n]:
+        ans = a[n]
+        a = a[n:]
+        it = len(a)
+    else:
+        ans = a[n]
+        break
+print(ans)
